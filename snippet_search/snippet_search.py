1f5f51dd8e09602206e0df29274b824211a0d4d1
LXNN <lxnn.git@gmail.com>
2022-05-14 16:03:23 +0100

    Cosmetic changes
    Added a separate embed to summarise the snippet search results.

--- snippet_search/snippet_search.py
+++ snippet_search/snippet_search.py
@@ -24,27 +24,33 @@ class SnippetSearch(commands.Cog):
             snippets = self.bot.snippets
         else:
             snippets = {
-                k: v
-                for k, v in self.bot.snippets.items()
-                if query.lower() in k
+                name: content
+                for name, content in self.bot.snippets.items()
+                if query.lower() in name
             }
 
         if not snippets:
             embed = discord.Embed(
-                description="No snippets found.",
+                description="No matching snippets found.",
                 color=self.bot.error_color,
             )
             await ctx.send(embed=embed)
             return
 
+        result_summary_embed = discord.Embed(
+            color=self.bot.main_color,
+            title=f"{len(snippets)} Matching Snippet{'s' if len(snippets) > 1 else ''}",
+            description=', '.join(f"`{name}`" for name in snippets),
+        )
+
         embeds = []
 
-        for name, val in snippets.items():
+        for i, (name, val) in enumerate(snippets.items(), start=1):
             content = truncate(escape_code_block(val), 2048 - 7)
             embed = (
                 discord.Embed(
-                    title=f'Snippets Found ({len(snippets)})',
                     color=self.bot.main_color,
+                    title="Snippet {i}",
                 )
                 .add_field(name="Name", value=f"`{name}`", inline=False)
                 .add_field(name="Raw Content", value=f"```\n{content}\n```", inline=False)
